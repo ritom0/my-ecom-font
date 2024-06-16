e7cc08e3e1c0f6e6ecdb9a945adfdeff16eafb73
ritom0 <143008523+ritom0@users.noreply.github.com>
2024-06-17 05:19:19 +0530

    Update UserAPI.js

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -11,7 +11,7 @@ const UserAPI = (token) => {
             const getUser = async () => {
                 try {
                     const res = await axios.get('https://my-ecom-back-13.onrender.com/user/infor', {
-                        headers: { Authorization: token }
+                        headers: { Authorization: Bearer ${token} }
                     });
 
                     setIsLogged(true);
