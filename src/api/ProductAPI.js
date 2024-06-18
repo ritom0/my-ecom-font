2eb74e565a4ea3d6ad864494a64d28a1a2c08380
ritom0 <143008523+ritom0@users.noreply.github.com>
2024-06-18 10:30:52 +0530

    Update ProductAPI.js

--- src/api/ProductAPI.js
+++ src/api/ProductAPI.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const ProductAPI = () => {
   const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
     try {
-      const res = await axios.get('https://my-ecom-back-13.onrender.com/api/products');
+      const res = await axios.get('${apiUrl}/api/products');
       setProducts(res.data.products); // Set the fetched products to the state
     } catch (error) {
       console.error('Failed to fetch products', error);
